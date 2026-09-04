3edd36ac767fbf4e1c9ff7de753097dace105176
zareiisakineh <zareiisakineh@gmail.com>
2026-09-04 11:12:01 +0200

    04_01_even_or_odd

--- tb_ex/ch_04_if_else/ex_04_01_even_or_odd_start.py
+++ tb_ex/ch_04_if_else/ex_04_01_even_or_odd_start.py
@@ -1,9 +1,13 @@
 # file: ex_04_01_even_or_odd_start.py
 
 # TODO: Read an integer from the user
-
+tall = int(input("Skriv et heltall: "))
 # TODO: Use the modulo operator (%) to check if the number is even or odd
 #       Hint: a number is even if number % 2 == 0
+if tall % 2 == 0:
+    print(f"{tall} er partall")
+else:
+    print(f"{tall} er oddetall")
 
 # TODO: Print the result
 #       Example: "42 is even."  or  "17 is odd."
